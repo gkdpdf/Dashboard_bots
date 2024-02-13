edcc2a4a775d0ffffaedf9054dfe4497ec9949f2
Gaurav Kumar <131261930+gkdpdf@users.noreply.github.com>
2024-02-13 18:00:34 +0530

    Add files via upload

---
+++ Dashboard_webapp.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+st.set_page_config(layout="wide")
+
+from pymongo.mongo_client import MongoClient
+
+
+
+#replace username and pwd from mongodb atlas
+uri = "mongodb+srv://readb:readb@cluster0.ay8hame.mongodb.net/?retryWrites=true&w=majority"
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+mydb_dashboard = client["Dashboard"]
+
+records =mydb_dashboard['records']   
+
+a=records.find()
+b=pd.DataFrame(a)
+b=b.drop("_id",axis=1)
+print(b)
+
+
+
+
+
+import plotly.express as px
+
+col1, col2 = st.columns([5, 5])
+with col1:
+    df = px.data.gapminder().query("country=='Canada'")
+    fig = px.line(b, x="Date String", y="Cumulative Profit", title='Total Profit')
+    st.plotly_chart(fig, theme="streamlit")
+
+with col2:
+    df = px.data.gapminder().query("country=='Canada'")
+    fig = px.line(b, x="Date String", y="ROI % till date", title='ROI %')
+    st.plotly_chart(fig, theme="streamlit")
+
+
+
+
+st.dataframe(b,width=2000)
