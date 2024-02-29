dd72b140f3d5b62f6fa5c0936bcb703ef77370bf
Gaurav Kumar <131261930+gkdpdf@users.noreply.github.com>
2024-02-29 17:52:34 +0530

    Update Dashboard_webapp.py

--- Dashboard_webapp.py
+++ Dashboard_webapp.py
@@ -56,7 +56,7 @@ with col2:
     # sns.lineplot(x='Date String', y='ROI % till date', data=b)
     # st.pyplot()
     # Compute the correlation matrix
-    c=b.drop(["Net Profit","ROI %","Cumulative Profit","ROI % till date"],axis=1)
+    c=b.drop(["Date String","Net Profit","ROI %","Cumulative Profit","ROI % till date"],axis=1)
     corr = c.corr()
     
     # Create a correlation heatmap using Seaborn
